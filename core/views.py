743d2edeabbbb79a31b120d7effaa7be3d908705
Kar Wi <t.karwi@yahoo.com>
2023-04-08 02:59:30 +0800

    chore: Remove saving prediction to db

--- core/views.py
+++ core/views.py
@@ -15,16 +15,6 @@ def predict_data_structure(request):
         if serializer.is_valid():
             # Perform your actual classification here
             dummy_prediction = {
-                # "array": 0.1,
-                # "string": 0.05,
-                # "linked_list": 0.02,
-                # "tree": 0.5,
-                # "graph": 0.03,
-                # "hash_table": 0.15,
-                # "heap": 0.02,
-                # "stack": 0.08,
-                # "queue": 0.03,
-                # "trie": 0.02,
                 "array": random.random(),
                 "string": random.random(),
                 "dynamic_programming": random.random(),
@@ -37,19 +27,13 @@ def predict_data_structure(request):
                 "binary_search": random.random(),
             }
 
-            # Create the Prediction object and save it
-            prediction = Prediction(
-                title=serializer.validated_data["title"],
-                description=serializer.validated_data["description"],
-                model_used=serializer.validated_data["model_used"],
-                prediction_result=dummy_prediction,
-            )
-            prediction.save()
+            response_data = {
+                "title": serializer.validated_data["title"],
+                "description": serializer.validated_data["description"],
+                "model_used": serializer.validated_data["model_used"],
+                "prediction_result": dummy_prediction,
+            }
 
-            # Serialize the saved Prediction object and return the response
-            response_serializer = PredictionSerializer(prediction)
-            return Response(
-                response_serializer.data, status=status.HTTP_201_CREATED
-            )
+            return Response(response_data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
